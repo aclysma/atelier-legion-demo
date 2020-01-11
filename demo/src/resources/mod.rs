0312a1bd92cb87af24de338d198b0367ac78715c
Philip Degarmo <aclysma@gmail.com>
2020-01-11 01:37:13 -0800

    Disable editor_draw for now, it's not fully hooked up and doesn't work

--- demo/src/resources/mod.rs
+++ demo/src/resources/mod.rs
@@ -44,7 +44,7 @@ mod debug_draw;
 pub use debug_draw::DebugDrawResource;
 pub use debug_draw::LineList;
 
-mod editor_draw;
-pub use editor_draw::EditorDrawResource;
-pub use editor_draw::EditorShapeClickedState;
-pub use editor_draw::EditorShapeDragState;
+//mod editor_draw;
+//pub use editor_draw::EditorDrawResource;
+//pub use editor_draw::EditorShapeClickedState;
+//pub use editor_draw::EditorShapeDragState;
